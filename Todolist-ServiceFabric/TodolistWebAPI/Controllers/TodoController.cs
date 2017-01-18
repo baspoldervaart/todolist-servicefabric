5fe746790f3354f21a1e8d75a7b7673ef7078e66
Bas Poldervaart <baspoldervaart92@hotmail.com>
2017-01-18 19:11:54 +0100

    Added TodoItem model & default WebAPI TodoController

---
+++ Todolist-ServiceFabric/TodolistWebAPI/Controllers/TodoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using TodolistWebAPI.Models;
+
+namespace TodolistWebAPI.Controllers
+{
+    public class TodoController : ApiController
+    {
+        // GET api/todo
+        public IEnumerable<TodoItem> Get()
+        {
+            //return new string[] { "value1", "value2" };
+            throw new NotImplementedException();
+        }
+
+        // GET api/todo/5 
+        public TodoItem Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        // POST api/todo 
+        public void Post([FromBody]TodoItem value)
+        {
+        }
+
+        // PUT api/todo/5 
+        public void Put(int id, [FromBody]TodoItem value)
+        {
+        }
+
+        // DELETE api/todo/5 
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Todolist-ServiceFabric/TodolistWebAPI/Models/TodoItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TodolistWebAPI.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Titel { get; set; }
+        public string Omschrijving { get; set; }
+        public string Prioriteit { get; set; }
+        public string ToegewezenAan { get; set; }
+        public DateTime VervalDatum { get; set; }
+        public string Categorie { get; set; }
+    }
+}
